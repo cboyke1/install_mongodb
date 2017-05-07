e7ff90ba7831ac9c0926cf5d4b44199ec8692fdf
cboyke1 <cboyke@gmail.com>
2017-05-07 16:49:02 -0700

    Added platform-specific commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,32 @@
-package 'mongodb' do
+case node[:platform]
+  when 'centos'
+		file '/etc/yum.repos.d/mongodb-org-3.4.repo' do
+			content '
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+		'
+		end
+  when 'ubuntu'
+      #getting permission denied
+      #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+    file '/etc/apt/sources.list.d/mongodb-org-3.4.list' do
+      content 'deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse'
+    end
+    apt_update 'update' do
+      action :update
+    end
+end
+
+package 'mongodb-org' do
+
 	action :install
 end
 
+service 'mongod' do
+  action [ :enable, :start ]
+end
